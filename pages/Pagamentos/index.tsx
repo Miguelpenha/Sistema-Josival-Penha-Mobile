11fb2368cf80881cf33d2ad748d2b9e87f9b1f7e
Miguel Penha <miguel.penha1@gmail.com>
2022-01-24 17:42:42 -0300

    teste

--- pages/Pagamentos/index.tsx
+++ pages/Pagamentos/index.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { get } from '../../api'
 import ContainerPd from '../../components/ContainerPd'
 import LoadingData from '../../components/loadingData'
 import { View, Text } from 'react-native'
 import HeaderBack from '../../components/HeaderBack'
-import DropDown from './DropDown'
 
 type RootStackParamList = {
     Pagamentos: undefined
@@ -15,23 +14,16 @@ type RootStackParamList = {
   
 type Iprops = NativeStackScreenProps<RootStackParamList, 'Pagamentos'>
 
-interface Ialuno {
-    nome: string
-    id: string
-}
-
 export default function Pagamentos({ navigation }: Iprops) {
-    const alunos: Ialuno[] = get('/alunos').data
-    const [aluno, setAluno] = useState(alunos && alunos[0])
+    const { data: turmas } = get('/turmas')
     
     return (
         <ContainerPd>
-            <LoadingData loading={alunos}>
+            <LoadingData loading={turmas}>
                 <View>
                     <HeaderBack onClick={() => navigation.goBack()}/>
                 </View>
-                <DropDown values={alunos} onChange={(aluno: Ialuno) => setAluno(aluno)}/>
-                <Text style={{color: '#ffffff', fontSize: 40, marginTop: '20%', alignSelf: 'center'}}>nome: {aluno.nome}</Text>
+                <Text style={{color: '#ffffff', fontSize: 50, marginTop: '20%', alignSelf: 'center'}}>Pagamentos</Text>
             </LoadingData>
         </ContainerPd>
     )
