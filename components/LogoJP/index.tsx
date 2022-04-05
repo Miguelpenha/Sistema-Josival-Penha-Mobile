43f4382d43a2e2273621fd982192a3ebb9af4215
Miguel Penha <miguel.penha1@gmail.com>
2022-04-05 16:32:46 -0300

    Update index.tsx

--- components/LogoJP/index.tsx
+++ components/LogoJP/index.tsx
@@ -4,12 +4,14 @@ import { Image } from 'react-native'
 
 const LogoJP: FC = (props) => {
     const theme = useTheme()
-    
+
     return (
         <Image
             {...props}
             source={
-                require(`../../assets/Logo-Josival-Penha-${theme.name === 'dark' ? 'Dark' : 'Light'}.png`)
+                theme.name == 'dark' ? 
+                require('../../assets/Logo-Josival-Penha-Dark.png') : 
+                require('../../assets/Logo-Josival-Penha-Light.png')
             }
         />
     )
